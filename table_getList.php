95992eeab84edec1bc9c659f4679f926e214b871
Lperke <juljagorb@gmail.com>
2017-04-21 13:18:19 +0300

    Update table_getList.php

--- table_getList.php
+++ table_getList.php
@@ -1,14 +1,36 @@
 <?
+
+use Bitrix\Highloadblock\HighloadBlockTable as HLBT;
+// https://dev.1c-bitrix.ru/api_d7/bitrix/highloadblock/index.php
+
 CModule::IncludeModule('highloadblock'); //модуль highload инфоблоков
 
 /*
 $HL_BLOCK_ID = 1;
 $hlblock = HLBT::getById($HL_BLOCK_ID)->fetch();
 $entity = HLBT::compileEntity($hlblock);
+$entity_data_class = $entity->getDataClass();
+
 var_dump($entity->getFields()); //all fields
 var_dump($entity_data_class::getCount()); //count
+
+$result = $entity_data_class::add(array( //add
+      'UF_NAME'         => 'Желтый',
+      'UF_CODE'         => 'YELLOW',
+      'UF_VALUE'         => '#ffff00',
+      'UF_ACTIVE'   => '1'
+   ));
+   
+   $result = $entity_data_class::delete($idForDelete); //remove
+   
+   $result = $entity_data_class::update($idForUpdate, array( //update
+  'UF_NAME'         => 'Фиолетовый',
+  'UF_CODE'         => 'PURPLE',
+  'UF_VALUE'         => '#5A009D',
+  'UF_ACTIVE'   => '1'
+));
 */
-   $rsData = \Bitrix\Highloadblock\HighloadBlockTable::getList(
+   $rsData = HLBT::getList(
      array(
        'filter'=>array(
          'TABLE_NAME'=> 'b_hlbd_somename'
@@ -21,7 +43,7 @@ var_dump($entity_data_class::getCount()); //count
    );
    
    if ( $hldata = $rsData->fetch() ){
-      $hlentity = \Bitrix\Highloadblock\HighloadBlockTable::compileEntity($hldata); //entity
+      $hlentity = HLBT::compileEntity($hldata); //entity
       $hlDataClass = $hldata['NAME'].'Table';                                       // data
    }
    else{
